dd79adaf1df6d75f5ffb25989f819c32a9155dca
hsbae <hellospring89@gmail.com>
2024-05-03 11:40:48 +0900

    RequestMapping을 이용한 스프링 MVC 구조 구현

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -11,4 +14,9 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	// @Bean
+	// SpringMemberFormControllerV1 springMemberFormControllerV1() {
+	// 	return new SpringMemberFormControllerV1();
+	// }
 }
